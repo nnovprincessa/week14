d3461bd0a0facd5e1f99290c29c78bdd65775435
Anna <nnovprincessa@gmail.com>
2022-12-18 18:27:14 +0300

    добавлен новый калькулятор

---
+++ new-calc.js
@@ -0,0 +1,27 @@
+function sum(){
+   let num1 = Number(document.getElementById('num1').value);
+   let num2 = Number(document.getElementById('num2').value);
+   result = num1+num2;
+   document.getElementById(`result`).innerHTML = result;
+}
+
+function subtraction(){
+    let num1 = Number(document.getElementById('num1').value);
+    let num2 = Number(document.getElementById('num2').value);
+    result = num1-num2;
+    document.getElementById(`result`).innerHTML = result;
+ }
+
+ function multiplication(){
+    let num1 = Number(document.getElementById('num1').value);
+    let num2 = Number(document.getElementById('num2').value);
+    result = num1*num2;
+    document.getElementById(`result`).innerHTML = result;
+ }
+
+ function division(){
+    let num1 = Number(document.getElementById('num1').value);
+    let num2 = Number(document.getElementById('num2').value);
+    result = num1/num2;
+    document.getElementById(`result`).innerHTML = result;
+ }
